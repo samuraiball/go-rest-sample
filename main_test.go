aacb2cc40f3bd011cb48a7a76256f11d42ceca39
yuya hirooka <yuy.hirohiro@gmail.com>
2019-11-12 22:04:49 +0900

    .gitignoreの追加

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
-	req, _ := http.NewRequest(method, "http://"+path, nil)
+	req, _ := http.NewRequest(method, "http://localhost"+path, nil)
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w
